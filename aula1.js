7c4f860d1f420230b6b9d859837fb166dbba3493
Lori-rls <lucaslealdes1@gmail.com>
2023-03-29 07:52:21 -0300

    Config inicial :P

---
+++ aula1.js
@@ -0,0 +1,22 @@
+let i
+var opa = "a"
+console.log(opa)
+
+var objeto = {
+    atributo1: 3,
+    atributo2: "string do objeto",
+    atributo3: ["array", "de", "string", "do", "objeto"],
+    atributo4: ["array", "de", "string", "do", "objeto", "com", "laço", "de", "repetição"]
+}
+
+console.log(objeto.atributo1)
+console.log(objeto.atributo2)
+console.log(objeto.atributo3[0], objeto.atributo3[1], objeto.atributo3[2], objeto.atributo3[3], objeto.atributo3[4], "\n")
+
+for(i=0; i<=10000; i++){
+    if(i%3==0){
+        console.log("O número", i, "é divisível por 3")
+    } else {
+        console.log("O número", i, "não é divisível por 3")
+    }
+}
